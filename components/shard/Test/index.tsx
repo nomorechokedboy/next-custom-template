636d5118332f4ae39921999cde2fe6dff5a740aa
nomorechokedboy <haiduongit@protonmail.com>
2021-12-03 12:14:00 +0700

    fix import styles from test component

--- components/shard/Test/index.tsx
+++ components/shard/Test/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from 'styles.module.scss';
+import styles from './styles.module.scss';
 
 export default function Test() {
     return <div className={styles.test}>Test</div>;
