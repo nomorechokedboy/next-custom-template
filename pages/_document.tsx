52a81424a3879ef4299adfbed1a3e540eb3ce930
nomorechokedboy <haiduongit@protonmail.com>
2022-02-18 20:23:29 +0700

    backup

---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+import Document, {
+    DocumentContext,
+    Head,
+    Html,
+    NextScript,
+    Main,
+} from 'next/document';
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx);
+
+        return { ...initialProps };
+    }
+
+    render() {
+        return (
+            <Html lang="vi">
+                <Head>
+                    <link rel="icon" href="static/pizza.svg" />
+                    <link
+                        rel="stylesheet"
+                        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
